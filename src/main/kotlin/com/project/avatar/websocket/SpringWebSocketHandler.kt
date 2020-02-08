c27924538724678e0f8b4df6f4008d1568331ffd
lovewlever <lovewlever@outlook.com>
2020-02-08 17:53:41 +0800

    增加一些东西

--- src/main/kotlin/com/project/avatar/websocket/SpringWebSocketHandler.kt
+++ src/main/kotlin/com/project/avatar/websocket/SpringWebSocketHandler.kt
@@ -1,28 +1,40 @@
 package com.project.avatar.websocket
 
-import org.springframework.web.socket.*
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketMessage
+import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
 
+/**
+ * WebSocket
+ */
 class SpringWebSocketHandler: TextWebSocketHandler() {
+
+    private val onlineUsers = HashMap<String,WebSocketSession>()
+
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
         super.afterConnectionClosed(session, status)
+        onlineUsers.remove(session.id)
     }
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
         super.afterConnectionEstablished(session)
+        onlineUsers[session.id] = session
     }
 
-    override fun handlePongMessage(session: WebSocketSession, message: PongMessage) {
-        super.handlePongMessage(session, message)
-    }
 
     override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
         super.handleMessage(session, message)
+        println("handleMessage:${message.payload}")
+        session.sendMessage(message)
     }
 
     override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
         super.handleTextMessage(session, message)
+        println("handleTextMessage")
     }
+
 
 }
